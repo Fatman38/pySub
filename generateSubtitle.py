9ef7c94b8408b2df17733b6e8eba8bd64de34ccb
Nicolas Montredon <nicolas.montredon@free.fr>
2021-11-25 23:22:03 +0100

    fix index pb
    it ain't much but it's honest work

--- generateSubtitle.py
+++ generateSubtitle.py
@@ -4,12 +4,14 @@
 
 # usage:
 # copy excel file in the same folder as this script AND rename to: "inputs.xlsx"
+#		rename all its column by its country code and rename the Time column by "Key"
 # copy each base str files in the same folder as this script AND rename each of them to "MODULE X.srt"
 # launch generateSubtitle.py
 
 import pandas as pd
 from pathlib import Path
 from itertools import islice
+import numpy as np
 
 def getDfFrom(filename):
 	myfile = open(filename, "r")
@@ -31,23 +33,25 @@ def getDfFrom(filename):
 def createSubtitleFile(timestamps, translation, column, modulename):
 	outputfilename = column + "-" + modulename + ".srt"
 	print(outputfilename)
-	#df = timestamps
 	
-	#print(df)
-	timestamps['Key'] = timestamps.index
-	df2 = translation.to_frame()
-	df2['Key'] = df2.index
-	print(df2)
-	timestamps['Key']=timestamps['Key'].astype(int)
-	df2['Key']=df2['Key'].astype(int)
+	df2 = translation
+
+	timestamps['Key']=timestamps['Key'].astype(str)
+	df2['Key']=df2['Key'].astype(str)
 
 	df3 = timestamps.merge(df2, how="outer", on='Key')
 	print(df3)
 	
-	outputfile = open(outputfilename, "w")
+	outputfile = open(outputfilename, "w", encoding='utf-8')
 	for index, row in df3.iterrows():
-		outputfile.write(str(row['Key']) + "\n")
-		outputfile.write(str(row['ts']) + "\n")
+		text = ""
+		if not pd.isnull(row['Key']):
+			text = str(row['Key'])
+		outputfile.write(text + "\n")
+		text = ""
+		if not pd.isnull(row['ts']):
+			text = str(row['ts'])
+		outputfile.write(text + "\n")
 		text = ""
 		if not pd.isnull(row[column]):
 			text = str(row[column])
@@ -75,11 +79,20 @@ for sheet in xl.sheet_names:
 	print(sdf)
 
 	df = pd.read_excel(xl, sheet_name=sheet)
+	df.dropna(subset = ['Key'], inplace=True)
 	print (df)
-	df2 = df.groupby(df.columns[0]).agg(lambda x : ' '.join(x))
-	print (df2)
+	if (df['Key'].dtypes != int):
+		df['Key'] = df['Key'].astype(np.int64)
+
+	newindex = df[df.columns[0]]
+	print(newindex)
+	df2 = df
+	df2 = df2.set_index(newindex)
+	print(df2)
 	print()
 	print()
 	
 	for column in df2:
-		createSubtitleFile(sdf, df2[column], column, sheet)
+		if column != 'Key':
+			createSubtitleFile(sdf, df[['Key', column]], column, sheet)
+		
